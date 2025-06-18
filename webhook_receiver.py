a4f6add1cfcf4184a98d8dbe5160cea8fba4a428
PSyuka <80306667+PSyuka@users.noreply.github.com>
2025-06-18 12:48:29 +0900

    Update webhook_receiver.py

--- webhook_receiver.py
+++ webhook_receiver.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route("/webhook", methods=['POST'])
 def webhook():
     body = request.get_json()
+    print("📦 受信データ：", json.dumps(body, indent=2, ensure_ascii=False))  # ← ここ追加！
+    
     events = body.get("events", [])
     for event in events:
         if event["type"] == "message":
@@ -15,3 +17,4 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
